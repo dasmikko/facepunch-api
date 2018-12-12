08f76a90711b95063f6efaea98965e33b46fe789
dasmikko <dasmikko@gmail.com>
2018-12-12 09:09:23 +0100

    Clean up some stuff, and added ops parsing

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 var restify = require("restify");
 const cheerio = require("cheerio");
-const createPhantomPool = require("phantom-pool");
+const axios = require("axios");
 
-console.log("Creating PhantomJS pool...");
-
-const pool = createPhantomPool({
-  max: 10, // default
-  min: 2, // default
-  // how long a resource can stay idle in pool before being removed
-  idleTimeoutMillis: 30000, // default.
-  // maximum number of times an individual resource can be reused before being destroyed; set to 0 to disable
-  maxUses: 50, // default
-  // function to validate an instance prior to use; see https://github.com/coopernurse/node-pool#createpool
-  validator: () => Promise.resolve(true), // defaults to always resolving true
-  // validate resource before borrowing; required for `maxUses and `validator`
-  testOnBorrow: true, // default
-  // For all opts, see opts at https://github.com/coopernurse/node-pool#createpool
-  phantomArgs: [["--ignore-ssl-errors=true", "--disk-cache=true"], {}] // arguments passed to phantomjs-node directly, default is `[]`. For all opts, see https://github.com/amir20/phantomjs-node#phantom-object-api
-});
 
 // Used for watinng JS load
 async function waitFor($config) {
@@ -39,152 +23,132 @@ async function waitFor($config) {
   setTimeout(waitFor, $config.interval || 0, $config);
 }
 
-async function phantomCall(url) {
-  return pool
-    .use(async instance => {
-      const page = await instance.createPage();
-      const status = await page.open(url, { operation: "GET" });
-      if (status !== "success") {
-        throw new Error("cannot open " + url);
+function parseOps(opsObject) {
+  parsedOps = [];
+
+  // Loop through the objects
+  opsObject.forEach(function(item, index) {
+    let lastParsedObj = parsedOps[parsedOps.length - 1]
+    let obj = item
+    let objContent = item.insert
+
+    // If first object just add it
+    if (index == 0) {
+      if (objContent.hasOwnProperty('hotlink')) {
+        parsedOps.push({
+          type: 'hotlink',
+          url: objContent.hotlink.url,
+          force: objContent.hotlink.force,
+          thumb: objContent.hotlink.thumb
+        })
+      } else if (objContent.hasOwnProperty('postquote')) {
+        let postquote = objContent.postquote
+        parsedOps.push({
+          type: 'postquote',
+          text: postquote.text,
+          forumid: postquote.forumid,
+          threadid: postquote.threadid,
+          postid: postquote.postid,
+          postnumber: postquote.postnumber,
+          username: postquote.username,
+          userid: postquote.userid
+        })
+      } else {
+        parsedOps.push({
+          type: 'text',
+          text: objContent,
+          attributes: obj.attributes ? obj.attributes : null,
+          children: []
+        })
       }
+    } else {
+      // Now we need to check if we need to add the current object to the last
+      // if it's a text object.
+      // hotlinks, postquotes etc, should be it's own seperate object
 
-      console.log("waiting for page loading");
-      let content = await waitFor({
-        debug: true, // optional
-        interval: 0, // optional
-        timeout: 1000, // optional
-        check: function() {
-          return page.evaluate(function() {
-            return $(".postrender").is(":visible");
-          });
-        },
-        success: async function() {
-          const content = await page.property("content");
-          return content;
-        },
-        error: function() {
-          console.log("Could not get content?");
-        } // optional
-      });
-      return content;
-    })
-    .then(content => {
-      return content;
-    });
-}
+      if (objContent.hasOwnProperty('hotlink')) {
+        parsedOps.push({
+          type: 'hotlink',
+          url: objContent.hotlink.url,
+          force: objContent.hotlink.force,
+          thumb: objContent.hotlink.thumb
+        })
+      } else if (objContent.hasOwnProperty('postquote')) {
+        let postquote = objContent.postquote
+        parsedOps.push({
+          type: 'postquote',
+          text: postquote.text,
+          forumid: postquote.forumid,
+          threadid: postquote.threadid,
+          postid: postquote.postid,
+          postnumber: postquote.postnumber,
+          username: postquote.username,
+          userid: postquote.userid
+        })
+      } else {
+        // Merge if same type
+        if (lastParsedObj.type === 'text') {
 
-async function phantontest(req, res, next) {
-  console.log("Fetching with phantom...");
-  let content = await phantomCall("https://forum.facepunch.com/f/");
+          // Handle list items
+          if (obj.attributes && obj.attributes.list) {
+            lastChildObj = lastParsedObj.children[lastParsedObj.children.length - 1]
 
-  var $ = cheerio.load(content);
+            // Check if it's the first child element
+            // If it is, add the attributes to the main text object
 
-  let items = [];
-  let subforums = [];
+            if (lastChildObj) {
+              // Is not the first child element
+              if (lastChildObj.hasOwnProperty('attributes') && lastChildObj.attributes !== null) {
+                lastParsedObj.children[lastParsedObj.children.length - 1].attributes.list = obj.attributes.list
+                delete obj.attributes.list
+              } else {
+                lastParsedObj.children[lastParsedObj.children.length - 1]['attributes'] = {
+                  list: obj.attributes.list
+                }
+                // Delete the old attributes
+                delete obj.attributes.list
+              }
+            } else {
+              // Is the first child element
+              if (lastParsedObj.hasOwnProperty('attributes') && lastParsedObj.attributes !== null) {
+                lastParsedObj.children[lastParsedObj.children.length - 1].attributes.list = obj.attributes.list
+                // Delete the old attributes
+                delete obj.attributes.list
+              } else {
+                lastParsedObj['attributes'] = {
+                  list: obj.attributes.list
+                }
+                // Delete the old attributes
+                delete obj.attributes.list
+              }
+            }
+          }
 
-  $(".forumblock").each(function(index, element) {
-    let id = $(this).find(".bglink").attr("href").substr(1);
-    let viewers = $(this).find(".forumtitle span").text();
-    $(this).find(".forumtitle span").remove(); // This is needed to get the title correctly.
-    let title = $(this).find(".forumtitle").text();
-    let icon = $(this).find("img").attr("src");
-
-    let subtitle = $(this)
-      .find(".forumsubtitle")
-      .text();
-
-    $(this)
-      .find(".threadcount")
-      .find(".label")
-      .remove();
-    let threadCount = parseInt(
-      $(this)
-        .find(".threadcount")
-        .attr("title")
-        .trim()
-        .slice(0, -8)
-    );
-
-    $(this)
-      .find(".postcount")
-      .find(".label")
-      .remove();
-    let postCount = parseInt(
-      $(this)
-        .find(".postcount")
-        .attr("title")
-        .trim()
-        .slice(0, -5)
-    );
-
-    let latestPost = $(this).find(".forumlastpost");
-    let latestPostTitle = latestPost
-      .find(".threadname")
-      .text()
-      .trim();
-    let latestPostUsername = latestPost.find(".username a").text();
-
-    let latestPostUserId = latestPost
-      .find(".username a")
-      .attr("href")
-      .substr(3);
-    latestPostUserId = latestPostUserId.slice(0, -1);
-
-    let latestPostDate = latestPost.find(".timeago").attr("title");
-    let latestPostTimeAgo = latestPost.find(".timeago").text();
-
-    // Loop subforums
-    $(this)
-      .find(".subforum")
-      .each(function(index, subforum) {
-        let subforumTitle = $(this)
-          .find("a")
-          .text();
-        let subforumId = $(this)
-          .find("a")
-          .attr("href")
-          .substr(3);
-        subforumId = subforumId.slice(0, -1);
-
-        subforums.push({
-          id: subforumId,
-          title: subforumTitle
-        });
-      });
-
-    items.push({
-      id: id,
-      icon: icon,
-      title: title.trim(),
-      subtitle: subtitle.trim(),
-      viewers: viewers,
-      threadCount: threadCount,
-      postCount: postCount,
-      latestPost: {
-        title: latestPostTitle,
-        timeAgo: latestPostTimeAgo,
-        date: latestPostDate,
-        user: {
-          id: latestPostUserId,
-          username: latestPostUsername
+          // Add child text element
+          lastParsedObj.children.push({
+            attributes: obj.attributes ? obj.attributes : null,
+            text: objContent
+          })
+        } else {
+          // Push new text obj
+          parsedOps.push({
+            type: 'text',
+            text: objContent,
+            attributes: obj.attributes ? obj.attributes : null,
+            children: []
+          })
         }
-      },
-      subforums: subforums
-    });
-  });
+      }
+    }
+  })
 
-  res.send(items);
-  next();
+  return parsedOps
 }
 
-function respond(req, res, next) {
-  res.send("hello " + req.params.name);
-  next();
-}
 
 async function forums(req, res, next) {
-  let content = await phantomCall("https://forum.facepunch.com/f/");
-  var $ = cheerio.load(content);
+  let content = await axios.get("https://forum.facepunch.com/f/");
+  var $ = cheerio.load(content.data);
   let categories = [];
 
   $(".forumcategoryblock").each(function(index, element) {
@@ -312,11 +276,11 @@ async function forums(req, res, next) {
 }
 
 async function forum(req, res, next) {
-  let content = await phantomCall(
+  let content = await axios.get(
     "https://forum.facepunch.com/f/" + req.params.forumid
-  );
+  )
 
-  var $ = cheerio.load(content);
+  var $ = cheerio.load(content.data);
 
   let threads = [];
 
@@ -365,12 +329,25 @@ async function forum(req, res, next) {
         .trim()
     );
 
-    $(this).find(".viewedcount>div>.label").remove(); // remove the label
+    $(this)
+      .find(".viewedcount>div>.label")
+      .remove(); // remove the label
     let viewedcount = parseInt(
-      $(this).find(".viewedcount").attr("title").replace(" members viewed this thread", ""));
+      $(this)
+        .find(".viewedcount")
+        .attr("title")
+        .replace(" members viewed this thread", "")
+    );
 
-    $(this).find(".postcount>div>.label").remove(); // remove the label
-    let postcount = parseInt($(this).find(".postcount>div").text().trim());
+    $(this)
+      .find(".postcount>div>.label")
+      .remove(); // remove the label
+    let postcount = parseInt(
+      $(this)
+        .find(".postcount>div")
+        .text()
+        .trim()
+    );
 
     let threadlastpost = $(this).find(".threadlastpost");
     let threadlastpostUsername = $(threadlastpost)
@@ -401,10 +378,19 @@ async function forum(req, res, next) {
 }
 
 async function thread(req, res, next) {
-  console.log('Fetching thread')
-  let content = await phantomCall("https://forum.facepunch.com/f/" + req.params.forumid + "/" + req.params.threadid1 + "/" + req.params.threadid2 + "/" + req.params.pagenumber);
+  console.log("Fetching thread");
+  let content = await axios.get(
+    "https://forum.facepunch.com/f/" +
+      req.params.forumid +
+      "/" +
+      req.params.threadid1 +
+      "/" +
+      req.params.threadid2 +
+      "/" +
+      req.params.pagenumber
+  );
 
-  var $ = cheerio.load(content);
+  var $ = cheerio.load(content.data);
 
   let posts = [];
 
@@ -413,89 +399,113 @@ async function thread(req, res, next) {
   let threadName = $(".threadname")
     .text()
     .trim();
+
   let currentPage = parseInt(req.params.pagenumber);
 
-  $(".postblock").each((index, element) => {
+  $("postrender").each((index, post) => {
     // Get username
-    let postUsername = $(element).find(".username>.inner>a").text().trim()
-    let postUserLevel = parseInt($(element).find('.postuser .inner>.userlevel').text().trim())
-    let postUserUrl = $(element).find(".username>.inner>a").attr('href')
-    let postUserAvatar = $(element).find('.postavatar>img').attr('src')
+    let postUsername = $(post).attr("username");
+    let postUserLevel = parseInt($(post).attr("level"));
+    let postUserUrl = $(post).attr("userurl");
+    let postUserAvatar = $(post).attr("avatar");
 
     // Get background image (if any)
     let backgroundImage = null;
-    if (bgimgRegex.exec($(element).find(".background-image").attr("style"))) {
-      backgroundImage = bgimgRegex.exec($(element).find(".background-image").attr("style"))[0]
-    }
 
-    let postContent = []
+    if ($(post).attr("userbackground"))
+      backgroundImage = $(post).attr("userbackground");
+    let postContent = JSON.parse($(post).attr("input"));
+    let parsedContent = parseOps(postContent.ops);
 
-    $(element).find('.ql-editor').children().each((index, contentElement) => {
-      let el = $(contentElement)
+    console.log(parsedContent)
 
-      // Check what root element it is
-      switch (contentElement.name) {
-        case 'p':
-          // Check if it contains any children
-          // And see if it's a link or br tag
-          if (el.children()[0]) {
-            switch (el.children()[0].name) {
-              case 'a':
-                postContent.push({
-                  type: 'link',
-                  url: $(el.children()[0]).attr('href'),
-                  content: $(el.children()[0]).text()
-                })
-                break;
-              case 'br':
-                postContent.push({
-                  type: 'newline',
-                })
-                break;
+    $(post)
+      .find(".ql-editor")
+      .children()
+      .each((index, contentElement) => {
+        let el = $(contentElement);
+
+        // Check what root element it is
+        switch (contentElement.name) {
+          case "p":
+            // Check if it contains any children
+            // And see if it's a link or br tag
+            if (el.children()[0]) {
+              switch (el.children()[0].name) {
+                case "a":
+                  postContent.push({
+                    type: "link",
+                    url: $(el.children()[0]).attr("href"),
+                    content: $(el.children()[0]).text()
+                  });
+                  break;
+                case "br":
+                  postContent.push({
+                    type: "newline"
+                  });
+                  break;
+              }
+            } else {
+              postContent.push({
+                type: "text",
+                content: el.text()
+              });
             }
-          } else {
+            break;
+          case "postquote":
             postContent.push({
-              type: 'text',
-              content: el.text()
-            })
-          }
-          break;
-        case 'postquote':
-          postContent.push({
-            type: 'postquote',
-            username: $(contentElement).attr('username'),
-            url: $(contentElement).find('a').attr('href'),
-            content: $(contentElement).find('.body').text()
-          })
-          break;
-        case 'hotlink':
-          let hotlinkType = null
+              type: "postquote",
+              username: $(contentElement).attr("username"),
+              url: $(contentElement)
+                .find("a")
+                .attr("href"),
+              content: $(contentElement)
+                .find(".body")
+                .text()
+            });
+            break;
+          case "hotlink":
+            let hotlinkType = null;
 
-          if ($(contentElement).find('iframe').length > 0) hotlinkType = 'iframe'
-          if ($(contentElement).find('.ec_youtube_root').length > 0) hotlinkType = 'youtube'
-          if ($(contentElement).find('video').length > 0) hotlinkType = 'video'
-          if ($(contentElement).find('.ec_image_root').length > 0) hotlinkType = 'image'
+            if ($(contentElement).find("iframe").length > 0)
+              hotlinkType = "iframe";
+            if ($(contentElement).find(".ec_youtube_root").length > 0)
+              hotlinkType = "youtube";
+            if ($(contentElement).find("video").length > 0)
+              hotlinkType = "video";
+            if ($(contentElement).find(".ec_image_root").length > 0)
+              hotlinkType = "image";
 
-          // A small "Hack" for twitch and twitter embeds...
-          // For some stupid reason, then just checking for iframe,
-          // it will not find it.
-          if($(contentElement).attr('url').includes('clips.twitch.tv')) hotlinkType = 'twitch-embed'
-          if($(contentElement).attr('url').includes('twitter.com')) hotlinkType = 'twitter-embed'
+            // A small "Hack" for twitch and twitter embeds...
+            // For some stupid reason, then just checking for iframe,
+            // it will not find it.
+            if (
+              $(contentElement)
+                .attr("url")
+                .includes("clips.twitch.tv")
+            )
+              hotlinkType = "twitch-embed";
+            if (
+              $(contentElement)
+                .attr("url")
+                .includes("twitter.com")
+            )
+              hotlinkType = "twitter-embed";
 
-          postContent.push({
-            type: 'hotlink',
-            hotlinkType: hotlinkType,
-            url: $(contentElement).attr('url'),
-          })
-          break;
-        default:
-          postContent.push({
-            type: 'text',
-            content: $(contentElement).text()
-          })
-          break;
-      }
-    })
+            postContent.push({
+              type: "hotlink",
+              hotlinkType: hotlinkType,
+              url: $(contentElement).attr("url")
+            });
+            break;
+          default:
+            postContent.push({
+              type: "text",
+              content: $(contentElement).text()
+            });
+            break;
+        }
+      });
 
     // Push the post to the list
     posts.push({
@@ -506,13 +516,15 @@ async function thread(req, res, next) {
         avatar: postUserAvatar ? postUserAvatar : null,
         backgroundImage: backgroundImage
       },
-      content: postContent
+      content: postContent,
+      contentParsed: parsedContent
     });
   });
 
   res.send({
     threadName: threadName,
     currentPage: currentPage,
+    totalPages: parseInt($(".pagnation.above>pagnation").attr("total")),
     posts: posts
   });
 
@@ -520,9 +532,6 @@ async function thread(req, res, next) {
 }
 
 var server = restify.createServer();
-server.get("/hello/:name", respond);
-server.head("/hello/:name", respond);
-
 // Home
 server.get("/", forums);
 server.head("/", forums);
@@ -541,10 +550,6 @@ server.get("/:forumid/:threadid1/:threadid2/:pagenumber", thread);
 server.head("/:forumid/:threadid1/:threadid2/:pagenumber", thread);
 server.get("/:forumid/:threadid1/:threadid2/:pagenumber/", thread);
 server.head("/:forumid/:threadid1/:threadid2/:pagenumber/", thread);
-
-// PHANTOM
-server.get("/phantom", phantontest);
-server.head("/phantom", phantontest);
 
 server.listen(8080, function() {
   console.log("NewPunch API started...");
